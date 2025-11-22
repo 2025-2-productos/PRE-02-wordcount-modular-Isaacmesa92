b9ba92153a5942f29e6ddc3e127f7371ce731b38
Isaac Mesa Maya <imesa@unal.edu.co>
2025-11-22 10:18:46 -0500

    Eliminacion variables magicas

--- homework/src/_internals/write_count_words.py
+++ homework/src/_internals/write_count_words.py
@@ -1,16 +1,13 @@
 import os
 
-def write_count_words(counter):
-    # Verifica si el directorio 'data/output' existe, si no, lo crea.
-    if not os.path.exists("data/output"):
-        os.makedirs("data/output")
+
+def write_count_words(counter, output_folder):
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
 
     # save the results using tsv format
-    # Abre el archivo 'data/output/results.tsv' en modo escritura ('w') con codificación UTF-8
-    with open("data/output/results.tsv", "w", encoding="utf-8") as f:
-        # Itera sobre los pares clave-valor (palabra y conteo) en el objeto 'counter'
+    output_path = os.path.join(output_folder, "results.tsv")
+    with open(output_path, "w", encoding="utf-8") as f:
         for key, value in counter.items():
             # write the key and value to the file
-            # Escribe la clave, un tabulador ('\t'), el valor y un salto de línea ('\n')
-            # Esto crea un formato TSV (Valores Separados por Tabuladores)
             f.write(f"{key}\t{value}\n")
--- homework/src/wordcount.py
+++ homework/src/wordcount.py
@@ -1,39 +1,22 @@
 # obtain a list of files in the input directory
 
-from homework.src._internals.count_words import count_words
-from homework.src._internals.preprocess_lines import preprocess_lines
-from homework.src._internals.read_all_lines import read_all_lines
-from homework.src._internals.split_into_words import split_into_words
-from homework.src._internals.write_count_words import write_count_words
+from ._internals.count_words import count_words
+from ._internals.preprocess_lines import preprocess_lines
+from ._internals.read_all_lines import read_all_lines
+from ._internals.split_into_words import split_into_words
+from ._internals.write_count_words import write_count_words
 
 
 def main():
-    ## read all lines
-    all_lines = read_all_lines()
+    input_folder = "data/input/"
+    output_folder = "data/output/"
 
-    ### preprocess lines
+    all_lines = read_all_lines(input_folder)
     all_lines = preprocess_lines(all_lines)
-
-    ### split in words
     words = split_into_words(all_lines)
-
-    ### count words
     counter = count_words(words)
-
-    write_count_words(counter)
+    write_count_words(counter, output_folder)
 
 
 if __name__ == "__main__":
     main()
-
-
-# create the directory output/ if it doesn't exist
-# def write_count_words():
-#     if not os.path.exists("data/output"):
-#         os.makedirs("data/output")
-
-#     # save the results using tsv format
-#     with open("data/output/results.tsv", "w", encoding="utf-8") as f:
-#         for key, value in counter.items():
-#             # write the key and value to the file
-#             f.write(f"{key}\t{value}\n")
